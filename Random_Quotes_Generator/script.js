3bbd925ba7f593c2485e5320058c91da2279e863
ketan <ketanpardhi04@gmail.com>
2024-06-14 12:24:47 +0530

    tweet out button

--- Random_Quotes_Generator/script.js
+++ Random_Quotes_Generator/script.js
@@ -1,3 +1,4 @@
+const twitterButton = document.querySelector("#js-tweet");
 const spinner = document.querySelector("#js-spinner");
 const newQuoteButton = document.querySelector("#js-new-quote");
 newQuoteButton.addEventListener("click", getQuote);
@@ -15,6 +16,7 @@ async function getQuote() {
     }
     const json = await response.json();
     displayQuote(json.message);
+    setTweetButton(json.message);
   } catch {
     alert("Failed to fetch new quote");
   } finally {
@@ -27,3 +29,12 @@ function displayQuote(quote) {
   const quoteText = document.querySelector("#js-quote-text");
   quoteText.textContent = quote;
 }
+
+function setTweetButton(quote) {
+  twitterButton.setAttribute(
+    "href",
+    `https://twitter.com/share?text=${quote} - Donald Trump`
+  );
+}
+
+getQuote();
