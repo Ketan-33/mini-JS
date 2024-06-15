2a60ca1175a712912235d331b719210588a840d1
ketan <ketanpardhi04@gmail.com>
2024-06-16 00:22:03 +0530

    loading spinner

--- Wikipedia_Search_App/script.js
+++ Wikipedia_Search_App/script.js
@@ -3,12 +3,25 @@ async function handleSubmit(event) {
   const inputValue = document.querySelector(".js-search-input").value;
   const searchQuery = inputValue.trim();
 
+  const searchResults = document.querySelector(".js-search-results");
+  searchResults.innerHTML = "";
+
+  const spinner = document.querySelector(".js-spinner");
+  spinner.classList.remove("hidden");
+
   try {
     const results = await searchWikipedia(searchQuery);
-    console.log(results);
+    if (results.query.searchinfo.totalhits === 0) {
+      alert("No results found. Try different keywords");
+      return;
+    }
+
+    displayResults(results);
   } catch (err) {
     console.log(err);
     alert("Failed to search wikipedia");
+  } finally {
+    spinner.classList.add("hidden");
   }
 }
 
@@ -20,6 +33,25 @@ async function searchWikipedia(searchQuery) {
   }
   const json = await response.json();
   return json;
+}
+
+function displayResults(results) {
+  const searchResults = document.querySelector(".js-search-results");
+
+  results.query.search.forEach((result) => {
+    const url = `https://en.wikipedia.org/?curid=${result.pageid}`;
+
+    searchResults.insertAdjacentHTML(
+      "beforeend",
+      `<div class="result-item">
+        <h3 class="result-title">
+          <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+        </h3>
+        <a href="${url}" class="result-link" target="_blank" rel="noopener">${url}</a>
+        <span class="result-snippet">${result.snippet}</span><br>
+      </div>`
+    );
+  });
 }
 
 const form = document.querySelector(".js-search-form");
