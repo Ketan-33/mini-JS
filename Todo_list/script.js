28b240e678d848878009dc8802c857a882b491f7
ketan <ketanpardhi04@gmail.com>
2024-06-17 12:50:07 +0530

    render todos

--- Todo_list/script.js
+++ Todo_list/script.js
@@ -1,5 +1,24 @@
 let todoItems = [];
 
+function renderTodo(todo) {
+  const list = document.querySelector(".js-todo-list");
+
+  const isChecked = todo.checked ? "done" : "";
+  const node = document.createElement("li");
+  node.setAttribute("class", `todo-item ${isChecked}`);
+  node.setAttribute("data-key", todo.id);
+  node.innerHTML = `
+    <input id="${todo.id}" type="checkbox"/>
+    <label for="${todo.id}" class="tick js-tick"></label>
+    <span>${todo.text}</span>
+    <button class="delete-todo js-delete-todo">
+    <svg><use href="#delete-icon"></use></svg>
+    </button>
+  `;
+
+  list.append(node);
+}
+
 function addTodo(text) {
   const todo = {
     text,
@@ -8,7 +27,7 @@ function addTodo(text) {
   };
 
   todoItems.push(todo);
-  console.log(todoItems);
+  renderTodo(todo);
 }
 
 const form = document.querySelector(".js-form");
