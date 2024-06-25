48c62be7ce437877ef5f46770cc5ababd187575c
ketan <ketanpardhi04@gmail.com>
2024-06-25 19:38:30 +0530

    start the timer

--- Pomodoro_Clock/main.js
+++ Pomodoro_Clock/main.js
@@ -5,8 +5,52 @@ const timer = {
   longBreakInterval: 4,
 };
 
+let interval;
+
+const mainButton = document.getElementById("js-btn");
+mainButton.addEventListener("click", () => {
+  const { action } = mainButton.dataset;
+  if (action === "start") {
+    startTimer();
+  }
+});
+
 const modeButtons = document.querySelector("#js-mode-buttons");
 modeButtons.addEventListener("click", handleMode);
+
+function getRemainingTime(endTime) {
+  const currentTime = Date.parse(new Date());
+  const difference = endTime - currentTime;
+
+  const total = Number.parseInt(difference / 1000, 10);
+  const minutes = Number.parseInt((total / 60) % 60, 10);
+  const seconds = Number.parseInt(total % 60, 10);
+
+  return {
+    total,
+    minutes,
+    seconds,
+  };
+}
+
+function startTimer() {
+  let { total } = timer.remainingTime;
+  const endTime = Date.parse(new Date()) + total * 1000;
+
+  mainButton.dataset.action = "stop";
+  mainButton.textContent = "stop";
+  mainButton.classList.add("active");
+
+  interval = setInterval(function () {
+    timer.remainingTime = getRemainingTime(endTime);
+    updateClock();
+
+    total = timer.remainingTime.total;
+    if (total <= 0) {
+      clearInterval(interval);
+    }
+  }, 1000);
+}
 
 function updateClock() {
   const { remainingTime } = timer;
@@ -43,3 +87,7 @@ function handleMode(event) {
 
   switchMode(mode);
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  switchMode("pomodoro");
+});
