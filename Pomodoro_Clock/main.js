9dd8924482616c9281ce3035c403f7e026277995
ketan <ketanpardhi04@gmail.com>
2024-06-27 20:28:44 +0530

    sound and progress bar

--- Pomodoro_Clock/main.js
+++ Pomodoro_Clock/main.js
@@ -8,8 +8,10 @@ const timer = {
 
 let interval;
 
+const buttonSound = new Audio("button-sound.mp3");
 const mainButton = document.getElementById("js-btn");
 mainButton.addEventListener("click", () => {
+  buttonSound.play();
   const { action } = mainButton.dataset;
   if (action === "start") {
     startTimer();
@@ -66,6 +68,8 @@ function startTimer() {
           switchMode("pomodoro");
       }
 
+      document.querySelector(`[data-sound="${timer.mode}"]`).play();
+
       startTimer();
     }
   }, 1000);
@@ -88,6 +92,13 @@ function updateClock() {
   const sec = document.getElementById("js-seconds");
   min.textContent = minutes;
   sec.textContent = seconds;
+
+  const text =
+    timer.mode === "pomodoro" ? "Get back to work!" : "Take a break!";
+  document.title = `${minutes}:${seconds} — ${text}`;
+
+  const progress = document.getElementById("js-progress");
+  progress.value = timer[timer.mode] * 60 - timer.remainingTime.total;
 }
 
 function switchMode(mode) {
@@ -103,6 +114,9 @@ function switchMode(mode) {
     .forEach((e) => e.classList.remove("active"));
   document.querySelector(`[data-mode="${mode}"]`).classList.add("active");
   document.body.style.backgroundColor = `var(--${mode})`;
+  document
+    .getElementById("js-progress")
+    .setAttribute("max", timer.remainingTime.total);
 
   updateClock();
 }
